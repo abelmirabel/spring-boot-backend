6de94e59942cf7fdc7206b9753bf85d843a56d3c
Daniel Luiz <abel.mirabel@gmail.com>
2019-01-15 22:02:38 -0200

    Criando repository e service para Categoria

---
+++ src/main/java/com/daniel/cursomc/repositories/CategoriaRepository.java
@@ -0,0 +1,15 @@
+package com.daniel.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.daniel.cursomc.domain.Categoria;
+
+/*
+ * so colocar a @Repository e extends JpaRepository<Categoria, Integer> onde integer é o id classe
+ */
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
+
+}
