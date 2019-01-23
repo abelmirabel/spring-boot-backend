de9c67bd8efe6544746eca357609a59561896131
Daniel Luiz <abel.mirabel@gmail.com>
2019-01-22 23:51:44 -0200

    Atualizacao: utilizando somente jsonIgnore

--- src/main/java/com/daniel/cursomc/repositories/ItemPedidoRepository.java
+++ src/main/java/com/daniel/cursomc/repositories/ItemPedidoRepository.java
@@ -3,7 +3,6 @@ package com.daniel.cursomc.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.daniel.cursomc.domain.Categoria;
 import com.daniel.cursomc.domain.ItemPedido;
 
 /*
