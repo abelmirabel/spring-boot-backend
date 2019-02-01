1486a7e7f1ad22a007dca2374440d58a50f8ce45
Daniel Luiz <abel.mirabel@gmail.com>
2019-02-01 00:18:02 -0200

    Criando profile de desenvolvimento

--- src/main/java/com/daniel/cursomc/services/DBService.java
+++ src/main/java/com/daniel/cursomc/services/DBService.java
@@ -59,7 +59,7 @@ public class DBService {
 	@Autowired
 	private ItemPedidoRepository itemPedidoRepository;
 	
-	public void instantiaateTestDatabase() throws ParseException {
+	public void instantiateTestDatabase() throws ParseException {
 		
 		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria(null, "Escritório");
